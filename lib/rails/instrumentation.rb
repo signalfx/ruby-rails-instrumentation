492e013aa83e914fa8e2ec31e5f2300e3b971a13
Ashwin Chandrasekar <achandrasekar@signalfx.com>
2019-01-30 12:31:22 -0500

    Add action_controller_subscriber

--- lib/rails/instrumentation.rb
+++ lib/rails/instrumentation.rb
@@ -1,8 +1,31 @@
 require 'rails/instrumentation/version'
 
+require 'rails/instrumentation/action_controller_subscriber'
+
 module Rails
   module Instrumentation
     class Error < StandardError; end
-    # Your code goes here...
+
+    class << self
+
+      attr_accessor :tracer
+
+      def instrument(tracer: OpenTracing.global_tracer)
+        @tracer = tracer
+        @subscriber_mutex = Mutex.new
+
+        @subscriber_mutex.synchronize do
+          add_subscribers
+        end
+      end
+
+      def add_subscribers
+        ActionControllerSubscriber.subscribe
+      end
+
+      def trace_event(event)
+        puts event
+      end
+    end
   end
 end
